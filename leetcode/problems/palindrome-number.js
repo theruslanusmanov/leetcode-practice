2c90af1e8c29b725290cc14d39e19c72efeb4e17
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-08-07 23:18:06 +0500

    add isPolindrome func

---
+++ leetcode/problems/palindrome-number.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+};
+
+describe('isPalindrome', () => {
+  /**
+   * Input: x = 121
+   * Output: true
+   */
+  it('should return true', () => {
+    expect(isPalindrome(121)).toBe(true)
+  })
+
+  /**
+   * Input: x = -121
+   * Output: false
+   * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+   */
+  it('should return false', () => {
+    expect(isPalindrome(-121)).toBe(false)
+  })
+
+  /**
+   * Input: x = 10
+   * Output: false
+   * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+   */
+  it('should return correct output', () => {
+    expect(isPalindrome(120)).toBe(21)
+  })
+
+  /**
+   * Input: x = -101
+   * Output: false
+   */
+  it('should return correct output', () => {
+    expect(isPalindrome(-101)).toBe(false)
+  })
+})
